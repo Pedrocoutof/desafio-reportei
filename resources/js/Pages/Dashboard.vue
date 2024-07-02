0c577039f1947b0164001faa0b3b466944b49b77
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-02 16:09:13 -0300

    Conectando busca de commit com o front

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,7 +8,7 @@ import Chart from "@/Components/Chart.vue";
 
 const userRepositories = ref();
 const selectedRepository = ref(null);
-let {loadingRepositories, loadingSelectedRepository} = ref(false);
+let {loadingRepositories, loadingSelectedRepository, loadingCommitData} = ref(false);
 const {props} = usePage();
 
 async function getRepositories() {
@@ -29,32 +29,23 @@ async function getRepositories() {
     return repositories || [];
 }
 
-async function getRepository() {
-    let repository;
-    loadingSelectedRepository = true;
+async function generateInsights() {
+    loadingCommitData = !loadingCommitData;
 
-    try {
-        const response = await axios.get("http://127.0.0.1:8000/api/repository", {
-            params: {
-                user: props.auth.user.nickname,
-                repository: selectedRepository.value
-            }
-        });
-
-        if (response.status === 200) {
-            repository = response.data;
+    axios.get("http://127.0.0.1:8000/api/commits", {
+        params: {
+            "user": props.auth.user.nickname,
+            "repository": selectedRepository.value
         }
-    } catch (error) {
-        console.error("Error fetching repositories:", error);
-    }
+    }).then((response) => {
+        console.log(response.data)
+    });
 
-    loadingSelectedRepository = false;
-    return repository || [];
+
 }
 
 onMounted(async () => {
-    // userRepositories.value = await getRepositories();
-    // console.log(userRepositories.value);
+    userRepositories.value = await getRepositories();
 });
 </script>
 
@@ -87,8 +78,8 @@ onMounted(async () => {
                                         </ul>
                                     </div>
                                 </div>
-                                <select v-model="selectedRepository" :disabled="loadingRepositories" @change="getRepository" id="countries" class="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-r-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option class="text-sm text-gray-600 " v-for="repository in userRepositories" :value="repository.name">{{ repository.name }}</option>
+                                <select v-model="selectedRepository" :disabled="loadingRepositories" id="countries" class="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-r-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option class="text-sm text-gray-600" v-for="repository in userRepositories" :value="repository.name">{{ repository.name }}</option>
                                 </select>
                                 <div v-if="loadingSelectedRepository" class="px-2" role="status">
                                     <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -97,7 +88,7 @@ onMounted(async () => {
                                     </svg>
                                     <span class="sr-only">Carregando...</span>
                                 </div>
-                                <button type="button" :disabled="!selectedRepository" class="mx-2 disabled:pointer-events-none disabled:dark:bg-green-900 disabled:bg-green-400 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Gerar Insights</button>
+                                <button @click="generateInsights" type="button" :disabled="!selectedRepository" class="mx-2 disabled:pointer-events-none disabled:dark:bg-green-900 disabled:bg-green-400 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Gerar Insights</button>
                             </div>
                         </form>
 
@@ -105,7 +96,6 @@ onMounted(async () => {
                 </div>
             </div>
         </div>
-
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
