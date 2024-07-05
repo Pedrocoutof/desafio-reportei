22ae716ff07014b068b1f092b8a0cb321e16c2a3
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-05 20:03:57 -0300

    Melhorando visualização no modo noturno

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -171,7 +171,7 @@ async function createChart()  {
                                 </div>
 
                                 <select :disabled="loadingRepositories" v-model="selectedRepository" id="countries" class="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-r-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2">
-                                    <option class="text-sm text-gray-600" v-for="repository in userRepositories" :key="repository.name" :value="repository.name">{{ repository.name }}</option>
+                                    <option class="text-sm text-gray-600 dark:text-gray-400" v-for="repository in userRepositories" :key="repository.name" :value="repository.name">{{ repository.name }}</option>
                                 </select>
                                 <CircleLoading v-if="loadingCommitData"></CircleLoading>
                                 <RefreshButton @click="updateRepositories"></RefreshButton>
