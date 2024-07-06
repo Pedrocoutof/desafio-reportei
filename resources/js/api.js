86edb3ea212df762cf143068fc140293605718a6
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-06 19:42:40 -0300

    Corrigindo acesso a api

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosPost = async (endpoint, params = null) => {
+    console.log(import.meta.env.VITE_API_BASE_URL + endpoint);
     return await axios.post(import.meta.env.VITE_API_BASE_URL + endpoint, params)
 }
 
