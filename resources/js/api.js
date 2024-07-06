267238df6c24b9e1f28b526a7cd427fcfb66d5cf
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-06 18:51:20 -0300

    Corrigindo rota para deploy

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosPost = async (endpoint, params = null) => {
-    return await axios.post("http://127.0.0.1:8000/api/" + endpoint, params)
+    return await axios.post("https://desafio-reportei-pedro.codejunior.com.br/api/" + endpoint, params)
 }
 
 export { axiosPost }
