17f943277d29774c1b3a4ba57d715b816d48ec6d
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-06 09:45:30 -0300

    Melhorando visual da página de login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -30,74 +30,78 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+    <Head title="Log in" />
 
+    <GuestLayout>
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
+
+        <h2 class="dark:text-gray-400 text-gray-600 p-6 text-center">
+            Gere métricas automatizadas dos seus repositórios!
+        </h2>
 
         <form :action="route('github.login')" class="text-center">
             <button type="submit" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2">
                 <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
                 </svg>
-                Sign in with Github
+                Entre com Github
             </button>
         </form>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+<!--        <form @submit.prevent="submit">-->
+<!--            <div>-->
+<!--                <InputLabel for="email" value="Email" />-->
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+<!--                <TextInput-->
+<!--                    id="email"-->
+<!--                    type="email"-->
+<!--                    class="mt-1 block w-full"-->
+<!--                    v-model="form.email"-->
+<!--                    required-->
+<!--                    autofocus-->
+<!--                    autocomplete="username"-->
+<!--                />-->
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+<!--                <InputError class="mt-2" :message="form.errors.email" />-->
+<!--            </div>-->
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+<!--            <div class="mt-4">-->
+<!--                <InputLabel for="password" value="Password" />-->
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
+<!--                <TextInput-->
+<!--                    id="password"-->
+<!--                    type="password"-->
+<!--                    class="mt-1 block w-full"-->
+<!--                    v-model="form.password"-->
+<!--                    required-->
+<!--                    autocomplete="current-password"-->
+<!--                />-->
 
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+<!--                <InputError class="mt-2" :message="form.errors.password" />-->
+<!--            </div>-->
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
-                </label>
-            </div>
+<!--            <div class="block mt-4">-->
+<!--                <label class="flex items-center">-->
+<!--                    <Checkbox name="remember" v-model:checked="form.remember" />-->
+<!--                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>-->
+<!--                </label>-->
+<!--            </div>-->
 
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                >
-                    Forgot your password?
-                </Link>
+<!--            <div class="flex items-center justify-end mt-4">-->
+<!--                <Link-->
+<!--                    v-if="canResetPassword"-->
+<!--                    :href="route('password.request')"-->
+<!--                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"-->
+<!--                >-->
+<!--                    Forgot your password?-->
+<!--                </Link>-->
 
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
+<!--                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">-->
+<!--                    Log in-->
+<!--                </PrimaryButton>-->
+<!--            </div>-->
+<!--        </form>-->
     </GuestLayout>
 </template>
