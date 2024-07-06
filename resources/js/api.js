cd8bb19265a995ff197684f344484136a807eb76
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-06 19:37:53 -0300

    Corrigindo acesso a api

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosPost = async (endpoint, params = null) => {
-    return await axios.post("https://desafio-reportei-pedro.codejunior.com.br/api/" + endpoint, params)
+    return await axios.post(import.meta.env.VITE_API_BASE_URL + endpoint, params)
 }
 
 export { axiosPost }
