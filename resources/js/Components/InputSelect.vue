bb3ed081f217df39d6c3b3d315dfdbc37052eb80
pedrocoutof <pedrocouto152001@gmail.com>
2024-07-06 15:20:35 -0300

    Implementando opção default no componente

--- resources/js/Components/InputSelect.vue
+++ resources/js/Components/InputSelect.vue
@@ -22,6 +22,7 @@ const updateValue = (event) => {
 
 <template>
     <select @change="updateValue" class="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-r-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2">
+        <option selected disabled class="text-sm text-gray-600 dark:text-gray-400">Selecione um repositório...</option>
         <option class="text-sm text-gray-600 dark:text-gray-400" v-for="option in props.options" :key="option.name" :value="option.name">{{ option.name }}</option>
     </select>
 </template>
